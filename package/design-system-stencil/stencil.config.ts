a5ab326bacc04862a34a5a0c7e9aed0294dab32f
Daniel Tedman <dbtedman@gmail.com>
2023-09-12 19:12:25 +1000

    Add Vue Wrapper

--- package/design-system-stencil/stencil.config.ts
+++ package/design-system-stencil/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { vueOutputTarget } from '@stencil/vue-output-target';
 
 export const config: Config = {
   namespace: 'design-system-stencil',
@@ -17,6 +18,10 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../design-system-vue/lib/components.ts',
+    }),
   ],
   testing: {
     browserHeadless: "new",
